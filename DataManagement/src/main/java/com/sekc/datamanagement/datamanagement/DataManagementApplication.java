f226096ad940baa180586cdb191745f98f5f44e0
hyh315 <2856400640@qq.com>
2022-10-02 16:04:26 +0800

    git repository init

---
+++ DataManagement/src/main/java/com/sekc/datamanagement/datamanagement/DataManagementApplication.java
@@ -0,0 +1,13 @@
+package com.sekc.datamanagement.datamanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataManagementApplication.class, args);
+    }
+
+}
---
+++ DataManagement/src/test/java/com/sekc/datamanagement/datamanagement/DataManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sekc.datamanagement.datamanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
