40b0a388fc9e481adc30fa0b8234079a2da4beee
hyh315 <2856400640@qq.com>
2022-10-09 21:13:20 +0800

    工具类、登陆注册controller文件创建

---
+++ DataManagementBackend/src/main/java/com/imis/datamanagement/controller/LoginController.java
@@ -0,0 +1,14 @@
+package com.imis.datamanagement.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/login")
+@Slf4j
+public class LoginController {
+
+}
