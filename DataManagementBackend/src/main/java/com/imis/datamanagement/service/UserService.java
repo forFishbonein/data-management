b9622c6829279956c5161cd609b4362cd6ac249c
hyh315 <2856400640@qq.com>
2022-10-09 23:57:59 +0800

    项目结构补充

---
+++ DataManagementBackend/src/main/java/com/imis/datamanagement/service/UserService.java
@@ -0,0 +1,10 @@
+package com.imis.datamanagement.service;
+
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.imis.datamanagement.common.R;
+import com.imis.datamanagement.domain.User;
+
+public interface UserService extends IService<User> {
+
+}
