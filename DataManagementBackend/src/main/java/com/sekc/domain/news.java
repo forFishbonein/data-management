1b6b31acf73324f115fe51a80e70b8ce778851ae
fangfei <2857313050@qq.com>
2022-10-09 23:37:08 +0800

    test

---
+++ DataManagementBackend/src/main/java/com/sekc/domain/news.java
@@ -0,0 +1,14 @@
+package com.sekc.domain;
+
+import java.util.Date;
+
+public class news {
+private Long userId;
+private String userEmail;
+private String userPass;
+private String userSid;
+private String userName;
+private Date createTime;
+private Date updateTime;
+private           deleted;
+}
