74803a7dc83a334dd0b9d02482cbf6859ecb95c3
dream12350 <3201613609@qq.com>
2022-10-09 20:57:35 +0800

    教师主页的导航

---
+++ DataManagementFrontend/IMIS-Frontend/src/components/TeacherNav.vue
@@ -0,0 +1,33 @@
+<template>
+ <div>
+   <header>
+     <TeacherNav_ />
+   </header>
+ </div>
+</template>
+
+<script>
+import TeacherNav_ from './TeacherNav_'
+export default {
+  name: 'TeacherNav',
+  components: {
+    TeacherNav_
+  }
+}
+</script>
+
+<style>
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  body {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  }
+  header {
+    background: dimgrey;
+    width: auto;
+    height: 48px;
+  }
+</style>
