2c01b8465dd6591f368f0abe58f9f2c4bd32f01b
forFishbonein <1558637209@qq.com>
2022-10-09 15:00:52 +0800

    ss

--- DataManagementBackend/src/main/java/com/sekc/datamanagement/DataManagementApplication.java
+++ DataManagementBackend/src/main/java/com/sekc/datamanagement/DataManagementApplication.java
@@ -2,8 +2,10 @@ package com.sekc.datamanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+//@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class DataManagementApplication {
 
     public static void main(String[] args) {
