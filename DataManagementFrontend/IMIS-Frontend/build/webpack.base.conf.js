41189e1013218397ea9d6ce96ca8a305b123d50f
forFishbonein <1558637209@qq.com>
2022-10-10 12:18:01 +0800

    a

--- DataManagementFrontend/IMIS-Frontend/build/webpack.base.conf.js
+++ DataManagementFrontend/IMIS-Frontend/build/webpack.base.conf.js
@@ -41,10 +41,6 @@ module.exports = {
   },
   module: {
     rules: [
-      // {
-      //   test: /\.scss$/,
-      //   loaders: ["style", "css", "sass"]
-      // },
       //...(config.dev.useEslint ? [createLintingRule()] : []),
       {
         test: /\.vue$/,
