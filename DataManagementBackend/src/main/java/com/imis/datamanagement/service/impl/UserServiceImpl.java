e4d7eb68b4fd1e0ac8bbbef7436200773ea2829c
hyh315 <2856400640@qq.com>
2022-10-10 23:38:57 +0800

    UserService 半成品

--- DataManagementBackend/src/main/java/com/imis/datamanagement/service/impl/UserServiceImpl.java
+++ DataManagementBackend/src/main/java/com/imis/datamanagement/service/impl/UserServiceImpl.java
@@ -1,14 +1,23 @@
 package com.imis.datamanagement.service.impl;
 
+import com.alibaba.druid.util.StringUtils;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.imis.datamanagement.common.result.CodeMsg;
 import com.imis.datamanagement.domain.User;
+import com.imis.datamanagement.exception.GlobalException;
 import com.imis.datamanagement.mapper.UserMapper;
 import com.imis.datamanagement.service.UserService;
+import com.imis.datamanagement.utils.MD5Util;
+import com.imis.datamanagement.utils.UUIDUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements UserService {
@@ -36,5 +45,97 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements Us
 
         //发送邮件操作，从 from到 to
         mailSender.send(mailMessage);
+    }
+
+    @Autowired
+    UserMapper userMapper;
+
+    @Autowired
+    RedisTemplate redisTemplate;
+
+    //cookie的名字
+    public static final String COOKIE_NAME_TOKEN = "token";
+
+    public User getByUserId(long userId) {
+
+        //取数据库
+        User user = userMapper.getById(id);
+        //再存入缓存
+        if (user != null) {
+            redisService.set(UserKey.getById, "" + id, user);
+        }
+        return user;
+    }
+
+    /**
+     * 典型缓存同步场景：更新密码
+     */
+    public boolean updatePassword(String token, long id, String formPass) {
+        //取user
+        User user = getById(id);
+        if(user == null) {
+            throw new GlobalException(CodeMsg.MOBILE_NOT_EXIST);
+        }
+        //更新数据库
+        User toBeUpdate = new User();
+        toBeUpdate.setId(id);
+        toBeUpdate.setPassword(MD5Util.formPassToDBPass(formPass, user.getSalt()));
+        userMapper.update(toBeUpdate);
+        //更新缓存：先删除再插入
+        redisService.delete(UserKey.getById, ""+id);
+        user.setPassword(toBeUpdate.getPassword());
+        redisService.set(UserKey.token, token, user);
+        return true;
+    }
+
+    public String login(HttpServletResponse response, LoginVo loginVo) {
+        if (loginVo == null) {
+            throw new GlobalException(CodeMsg.SERVER_ERROR);
+        }
+        String mobile = loginVo.getMobile();
+        String formPass = loginVo.getPassword();
+        //判断手机号是否存在
+        User user = getById(Long.parseLong(mobile));
+        if (user == null) {
+            throw new GlobalException(CodeMsg.MOBILE_NOT_EXIST);
+        }
+        //验证密码
+        String dbPass = user.getPassword();
+        String saltDB = user.getSalt();
+        String calcPass = MD5Util.formPassToDBPass(formPass, saltDB);
+        if (!calcPass.equals(dbPass)) {
+            throw new GlobalException(CodeMsg.PASSWORD_ERROR);
+        }
+        //生成唯一id作为token
+        String token = UUIDUtil.uuid();
+        addCookie(response, token, user);
+        return token;
+    }
+
+    /**
+     * 将token做为key，用户信息做为value 存入redis模拟session
+     * 同时将token存入cookie，保存登录状态
+     */
+    public void addCookie(HttpServletResponse response, String token, User user) {
+        redisService.set(UserKey.token, token, user);
+        Cookie cookie = new Cookie(COOKIE_NAME_TOKEN, token);
+        cookie.setMaxAge(UserKey.token.expireSeconds());
+        cookie.setPath("/");//设置为网站根目录
+        response.addCookie(cookie);
+    }
+
+    /**
+     * 根据token获取用户信息
+     */
+    public User getByToken(HttpServletResponse response, String token) {
+        if (StringUtils.isEmpty(token)) {
+            return null;
+        }
+        User user = redisService.get(UserKey.token, token, User.class);
+        //延长有效期，有效期等于最后一次操作+有效期
+        if (user != null) {
+            addCookie(response, token, user);
+        }
+        return user;
     }
 }
