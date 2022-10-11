c12633f50681ad462b8e4e48d8313a377d4bed70
hyh315 <2856400640@qq.com>
2022-10-11 18:54:03 +0800

    feat:新增"验证码错误"的codeMsg

--- DataManagementBackend/src/main/java/com/imis/datamanagement/service/UserService.java
+++ DataManagementBackend/src/main/java/com/imis/datamanagement/service/UserService.java
@@ -5,8 +5,6 @@ import com.baomidou.mybatisplus.extension.service.IService;
 import com.imis.datamanagement.domain.User;
 
 public interface UserService extends IService<User> {
-    //发送邮件
-    void sendEmail(String to, String subject, String context);
 
     User getByUserId(long id);
 }
