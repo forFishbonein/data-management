d2f0524317d4dc043bf7d16128d768fbd77597af
hyh315 <2856400640@qq.com>
2022-10-12 20:37:26 +0800

    fix:启动类bug

--- DataManagementBackend/src/main/java/com/imis/datamanagement/service/EmailService.java
+++ DataManagementBackend/src/main/java/com/imis/datamanagement/service/EmailService.java
@@ -10,7 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
 
+@Service
 public class EmailService {
 
     // 邮件发送人
