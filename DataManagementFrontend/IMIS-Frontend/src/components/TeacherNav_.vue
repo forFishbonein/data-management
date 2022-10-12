1d9150b09c458fec1b2840ae13885cb9fd0d3858
dream12350 <3201613609@qq.com>
2022-10-12 20:15:29 +0800

    更改颜色字体

--- DataManagementFrontend/IMIS-Frontend/src/components/TeacherNav_.vue
+++ DataManagementFrontend/IMIS-Frontend/src/components/TeacherNav_.vue
@@ -23,6 +23,7 @@ nav {
   align-items: center;
   justify-content: center; 
   width: 100%;
+  background-color: #FFFFFF;
 }
 
 nav .logo {
@@ -36,19 +37,19 @@ nav .logo {
 }
 
 nav .el-menu-resource {
-  color: #FFFFFF;
+  color: #3C85D7;
   padding: 15px 50px;
   margin-left:10px;
   margin-right:10px;
   position: relative;
   text-align: center;
-  border-bottom: transparent;
   display: flex;
-  transition: 0.4s;
   font-size: larger;
   font-weight: bold;
 }
 nav .el-menu-resource:hover {
+  text-decoration-line: underline;
+  border-bottom: 1px solid #3C85D7;
   cursor: pointer; 
   color: #409EFF; 
 }
@@ -56,35 +57,34 @@ nav .el-menu-resource:hover {
 nav .el-menu-search {
   color: #FFFFFF;
   width: 450px;
+  height: 35px;
   padding: 15px 50px;
   margin-left:50px;
   margin-right:50px;
   position: relative;
   display: flex;
-  transition: 0.4s;
   font-size: larger;
   font-weight: bold;
 }
 nav .searchInput {
   width: 450px;
-  text-align: left;
-  line-height: 1rem;
+  height: 35px;
   border:#ccc 1px solid;
   text-align: center;
+  position: relative;
+  display: flex;
   font-size: 18px;
   border-radius: 10px;
 }
 
 nav .el-menu-center {
-  color: #FFFFFF;
+  color: #3C85D7;
   padding: 15px 50px;
   margin-left:10px;
   margin-right:10px;
   position: relative;
   text-align: center;
-  border-bottom: transparent;
   display: flex;
-  transition: 0.4s;
   font-size: larger;
   font-weight: bold;
 }
@@ -108,9 +108,10 @@ nav .el-menu-push {
   transition: 0.4s;
   font-size: larger;
   font-weight: bold;
+  background-color: #3C85D7;
 }
 nav .el-menu-push:hover {
-  background-color: #7A91BC;
+  background-color: #1E5594;
   /* height: 48px; */
   cursor: pointer;
 
