db6cd16e9c3a9b57eb84e38b0e26fbb8ec52ea14
syl <937655433@qq.com>
2022-10-12 19:21:14 +0800

    教师端注册页面——增加获取验证码按钮

--- DataManagementFrontend/IMIS-Frontend/src/views/RegisterTeacher.vue
+++ DataManagementFrontend/IMIS-Frontend/src/views/RegisterTeacher.vue
@@ -5,28 +5,26 @@
     </div>
     <div class="body">
       <form class="right">
-        <div class="box"><input class="container" type="text" placeholder="请输入邮箱"></div>
+        <div class="box"><input class="containerfirst" type="text" placeholder="请输入邮箱"><button class="buttonin">获取验证码</button></div>
         <div class="box"><input class="container" type="text" placeholder="请输入验证码"></div>
         <div class="box"><input class="container" type="text" placeholder="请输入姓名"></div>
         <div class="box"><input class="container" type="text" placeholder="请输入教工号"></div>
         <div class="box"><input class="container" type="text" placeholder="请输入密码"></div>
         <div class="box"><input class="container" type="text" placeholder="请再次输入密码"></div>
-        <div class="box">
-          <button class="button">注册</button>
-        </div>
+        <div class="box"><button class="button">注册</button></div>
       </form>
     </div>
     <div class="footer">
-      邮箱：{{ email }}&emsp;大数据实验班版权所有
+      邮箱：{{email}}&emsp;大数据实验班版权所有
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  data() {
+  data () {
     return {
-      email: "256235478@qq.com"
+      email:"256235478@qq.com"
     }
   }
 }
@@ -34,19 +32,17 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-* {
+*{
   margin: 0;
 }
-
-.main {
+.main{
   width: 1200px;
   height: 100vh;
   flex-direction: column;
   align-items: center;
   margin: auto;
 }
-
-.header {
+.header{
   background-color: cornflowerblue;
   color: white;
   display: flex;
@@ -54,16 +50,14 @@ export default {
   align-items: center;
   height: 100px;
 }
-
-.body {
+.body{
   background-color: azure;
   height: 500px;
   display: flex;
   padding-top: 50px;
   padding-bottom: 50px;
 }
-
-.footer {
+.footer{
   background-color: cornflowerblue;
   color: white;
   display: flex;
@@ -71,8 +65,7 @@ export default {
   align-items: center;
   height: 100px;
 }
-
-.right {
+.right{
   background-color: white;
   height: 100%;
   width: 500px;
@@ -82,20 +75,26 @@ export default {
   flex-direction: column;
   align-items: center;
 }
-
-.box {
+.box{
   font-size: 16px;
   margin-top: 5px;
 }
-
-.box span {
-  display: inline-block;
-  text-align: right;
-  width: 100px;
-  height: 53px;
+.containerfirst{
+  width: 200px;
+  height: 30px;
+  padding: 0 2vh;
+  margin-top: 15px;
+  border: 1.5px solid #7A91BC;
+  opacity: 1;
+  border-radius: 8px;
+  background: rgba(255, 255, 255, 1);
 }
-
-.container {
+.buttonin{
+  width: 95px;
+  height: 30px;
+  margin-left: 5px;
+}
+.container{
   width: 300px;
   height: 30px;
   padding: 0 2vh;
@@ -105,8 +104,7 @@ export default {
   border-radius: 8px;
   background: rgba(255, 255, 255, 1);
 }
-
-.button {
+.button{
   width: 300px;
   height: 40px;
   color: white;
