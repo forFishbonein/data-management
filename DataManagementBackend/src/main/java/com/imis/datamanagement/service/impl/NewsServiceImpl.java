e0689da6290638a36992b0c04e1a3df187a6d412
fangfei <2857313050@qq.com>
2022-10-16 10:21:01 +0800

    feat：修改NewService

--- DataManagementBackend/src/main/java/com/imis/datamanagement/service/impl/NewsServiceImpl.java
+++ DataManagementBackend/src/main/java/com/imis/datamanagement/service/impl/NewsServiceImpl.java
@@ -1,6 +1,8 @@
 package com.imis.datamanagement.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.imis.datamanagement.domain.News;
+
 import com.imis.datamanagement.mapper.NewsMapper;
 import com.imis.datamanagement.service.NewsService;
 
@@ -12,12 +14,16 @@ public class NewsServiceImpl implements NewsService {
     @Resource
     NewsMapper newsMapper;
     @Override
-    public void getById(long id) {
-
+    public News getById(long id) {
+        QueryWrapper<News> queryWrapper = new QueryWrapper<>();
+        queryWrapper.lambda().eq(News::getNewsId, id);
+        News onenews = newsMapper.selectOne(queryWrapper);
+        return onenews;
     }
 
     @Override
     public List<News> getAllNews() {
-return null;
+
+        return null;
     }
 }
