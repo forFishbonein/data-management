15e1999edc2afbdd4733a5bec27285bc07a9a16a
gyy <2086876431@qq.com>
2022-10-16 21:26:15 +0800

    feat

--- DataManagementBackend/src/main/java/com/imis/datamanagement/common/result/CodeMsg.java
+++ DataManagementBackend/src/main/java/com/imis/datamanagement/common/result/CodeMsg.java
@@ -23,8 +23,8 @@ public class CodeMsg {
     public static CodeMsg EMAIL_EXIST = new CodeMsg(500219, "用户已存在");
     public static CodeMsg FILE_NOT_EXIST = new CodeMsg(500220, "文件不存在或已删除");
     public static CodeMsg FILE_EXIST = new CodeMsg(500220, "文件已存在");
-
-    public static CodeMsg ID_NOT_EXIST = new CodeMsg(500300, "新闻不存在");
+    //新闻模块 5003XX
+    public static CodeMsg NEWS_NOT_EXIST = new CodeMsg(500300, "新闻不存在");
 
 
 //
