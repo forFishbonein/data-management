e4958cf5691cfbf749f3486ee73f75dc9bc8eaeb
lx <1164475931@qq.com>
2022-10-18 19:14:59 +0800

    fix:教师主页修复

--- DataManagementFrontend/IMIS-Frontend/src/App.vue
+++ DataManagementFrontend/IMIS-Frontend/src/App.vue
@@ -1,13 +1,5 @@
 <template>
   <div id="app">
-<<<<<<< HEAD
-<<<<<<< HEAD
-    <TeacherIndex></TeacherIndex>
-    <TeacherIndex></TeacherIndex>
-=======
-<<<<<<< HEAD
-=======
->>>>>>> bdaa2e4fe1a8b4f7e351a68d2bc5d8d401008e1e
    <TeacherIndex></TeacherIndex>
 <!--     <RegisterTeacher></RegisterTeacher>-->
 <!--     <RegisterStudent></RegisterStudent>-->
@@ -17,21 +9,7 @@
 <!--     <SearchResults></SearchResults>-->
 <!--    <test></test>-->
     <!-- <LoginFooter></LoginFooter> -->
-<<<<<<< HEAD
-=======
-     <TeacherIndex></TeacherIndex>
->>>>>>> aa9f786e38ff4a8ce0f1854aa2aa4ba83a039e9e
-    <!-- <RegisterTeacher></RegisterTeacher> -->
-    <!-- <RegisterStudent></RegisterStudent> -->
-    <!-- <StudentIndex></StudentIndex> -->
-    <!-- <Manage></Manage> -->
-    <!-- <Compete></Compete> -->
-    <!-- <SearchResults></SearchResults> -->
-    <!-- <test></test> -->
->>>>>>> 1eaf1926808391991ef216b01a46ce6d05f6a110
-=======
 
->>>>>>> bdaa2e4fe1a8b4f7e351a68d2bc5d8d401008e1e
   </div>
 </template>
 
