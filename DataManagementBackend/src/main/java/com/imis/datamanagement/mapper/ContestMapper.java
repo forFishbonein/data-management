ac45c81db0129d4d9cdac43b60d26e5acdd59710
fangfei <2857313050@qq.com>
2022-10-18 16:34:11 +0800

    feat：修改ContestMapper

--- DataManagementBackend/src/main/java/com/imis/datamanagement/mapper/ContestMapper.java
+++ DataManagementBackend/src/main/java/com/imis/datamanagement/mapper/ContestMapper.java
@@ -3,6 +3,8 @@ package com.imis.datamanagement.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.imis.datamanagement.domain.Contest;
 import com.imis.datamanagement.domain.TeacherInfo;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface ContestMapper extends BaseMapper<Contest> {
 }
