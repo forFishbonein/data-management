b127341f9acf29bb1a0db41d7c17e6e7459a0f42
hyh315 <2856400640@qq.com>
2022-10-18 16:46:47 +0800

    style:新闻RESTful风格

--- DataManagementBackend/src/main/java/com/imis/datamanagement/controller/NewsController.java
+++ DataManagementBackend/src/main/java/com/imis/datamanagement/controller/NewsController.java
@@ -5,7 +5,6 @@ import com.imis.datamanagement.common.result.Result;
 import com.imis.datamanagement.domain.News;
 import com.imis.datamanagement.service.NewsService;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.ibatis.annotations.Delete;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -21,7 +20,7 @@ public class NewsController {
 
     //根据id查询
     @GetMapping("/{newsId}")
-    public Result<News> getById(@PathVariable("newsId") Long newsId){
+    public Result<News> getById(@PathVariable("newsId") Long newsId) {
         News news = newsService.getById(newsId);
         if (news == null) {
             return Result.error(CodeMsg.NEWS_NOT_EXIST);
@@ -31,29 +30,29 @@ public class NewsController {
 
     //查询全部
     @GetMapping
-    public Result<List<News>> getAllNews(){
+    public Result<List<News>> getAllNews() {
         List<News> news = newsService.getAllNews();
-          return Result.success(news);
+        return Result.success(news);
     }
 
     //增加一条新闻
-      @PostMapping("/insert")
-     public Result<String> insert(@RequestBody News news){
+    @PutMapping
+    public Result<String> insert(@RequestBody News news) {
         newsService.insert(news);
         return Result.success("新增成功");
-      }
+    }
 
-      //根据id删除
-      @DeleteMapping("/delete/{newsId}")
-       public Result<String> deleteById(@PathVariable("newsId") Long newsId){
+    //根据id删除
+    @DeleteMapping("/{newsId}")
+    public Result<String> deleteById(@PathVariable("newsId") Long newsId) {
         newsService.deleteById(newsId);
-         return Result.success("删除成功");
-      }
+        return Result.success("删除成功");
+    }
 
-      //根据id修改
-      @PutMapping("/update/{newsId}")
-         public Result<String> updateById(@PathVariable("newsId") Long newsId){
-          newsService.updateById(newsId);
-          return Result.success("修改成功");
-      }
+    //根据id修改
+    @PostMapping("/{newsId}")
+    public Result<String> updateById(@PathVariable("newsId") Long newsId) {
+        newsService.updateById(newsId);
+        return Result.success("修改成功");
+    }
 }
