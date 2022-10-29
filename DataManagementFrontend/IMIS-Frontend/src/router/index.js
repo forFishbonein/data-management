9e74e8e64bcd65e405af9d5775504ee47277d5fe
forFishbonein <1558637209@qq.com>
2022-10-29 15:28:25 +0800

    Merge branch 'master' of https://gitee.com/hyh315/DataManagement4Sek-c
    # Conflicts:
    #	DataManagementFrontend/IMIS-Frontend/src/App.vue
    #	DataManagementFrontend/IMIS-Frontend/src/main.js
    #	DataManagementFrontend/IMIS-Frontend/src/request/index.js
    #	DataManagementFrontend/IMIS-Frontend/src/store/index.js
    #	DataManagementFrontend/IMIS-Frontend/src/views/RegisterTeacher.vue

--- DataManagementFrontend/IMIS-Frontend/src/router/index.js
+++ DataManagementFrontend/IMIS-Frontend/src/router/index.js
@@ -234,43 +234,43 @@ const router = new VueRouter({
   routes
 });
 
-// router.beforeEach((to, from, next) => {
-//   if (getToken()) {
-//     if (to.path === "/login") {
-//       next({ path: "/" });
-//     } else {
-//       //如果不是跳转到登录页面！那么获取用户信息！
-//       if (store.state.userEmail.length === 0) {
-//         //如果还没有用户信息
-//         store
-//           .dispatch("getUserInfo") //这里调用了获取用户信息
-//           .then(data => {
-//             //获取用户信息
-//             next();
-//           })
-//           .catch(() => {
-//             Message({
-//               type: "warning",
-//               showClose: true,
-//               message: "登录已过期"
-//             });
-//             next({ path: "/" });
-//           });
-//       } else {
-//         next();
-//       }
-//     }
-//   } else {
-//     if (to.matched.some(r => r.meta.requireLogin)) {
-//       Message({
-//         type: "warning",
-//         showClose: true,
-//         message: "请先登录哦"
-//       });
-//     } else {
-//       next();
-//     }
-//   }
-// });
+router.beforeEach((to, from, next) => {
+  if (getToken()) {
+    if (to.path === "/") { //如果是跳转到登录页面，那么不拦截
+      next({ path: "/" });
+    } else {
+      //如果不是跳转到登录页面！那么获取用户信息！
+      if (store.state.userEmail.length === 0) {
+        //如果还没有用户信息
+        store
+          .dispatch("getUserInfo") //获取用户信息
+          .then(data => {
+            //获取用户信息
+            next();
+          })
+          .catch(() => {
+            Message({
+              type: "warning",
+              showClose: true,
+              message: "登录已过期"
+            });
+            next({ path: "/" });
+          });
+      } else {
+        next();
+      }
+    }
+  } else {
+    if (to.matched.some(r => r.meta.requireLogin)) {
+      Message({
+        type: "warning",
+        showClose: true,
+        message: "请先登录哦"
+      });
+    } else {
+      next();
+    }
+  }
+});
 
 export default router;
