9cd32adfa0c77c961ee924194bb3794e3858a403
hyh315 <2856400640@qq.com>
2022-10-31 16:52:38 +0800

    fix:commu

--- DataManagementFrontend/IMIS-Frontend/src/views/template/Communication.vue
+++ DataManagementFrontend/IMIS-Frontend/src/views/template/Communication.vue
@@ -63,7 +63,7 @@ export default {
         name: "",
         organizer: "",
         address: "",
-        member: "",
+        member: [],
         whetherSpeak: "",
         whetherParticipate: "",
 
