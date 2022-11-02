90e7497e80913bc466780d8449c35841f51f2438
forFishbonein <1558637209@qq.com>
2022-11-02 12:32:14 +0800

    Merge branch 'master' of https://gitee.com/hyh315/DataManagement4Sek-c
    # Conflicts:
    #	DataManagementFrontend/IMIS-Frontend/src/App.vue
    #	DataManagementFrontend/IMIS-Frontend/src/main.js
    #	DataManagementFrontend/IMIS-Frontend/src/request/index.js
    #	DataManagementFrontend/IMIS-Frontend/src/store/index.js
    #	DataManagementFrontend/IMIS-Frontend/src/views/RegisterTeacher.vue

--- DataManagementBackend/src/test/java/com/imis/datamanagement/datamanagement/DataManagementApplicationTests.java
+++ DataManagementBackend/src/test/java/com/imis/datamanagement/datamanagement/DataManagementApplicationTests.java
@@ -1,6 +1,6 @@
 package com.imis.datamanagement.datamanagement;
 
-import com.imis.datamanagement.service.EmailService;
+//import com.imis.datamanagement.service.EmailService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,8 +15,6 @@ class DataManagementApplicationTests {
 
     @Test
     public void contextLoads() {
-
-
 
 
 
